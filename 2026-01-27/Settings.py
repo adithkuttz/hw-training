6b6a1b6992895c3dda1d9d8ce588d41167bb4bd4
adithkuttz <162976483+adithkuttz@users.noreply.github.com>
2026-01-27 22:39:32 +0530

    Create Settings.py

---
+++ 2026-01-27/Settings.py
@@ -0,0 +1,14 @@
+
+HEADLESS = True
+PAGE_TIMEOUT = 60000
+SLOW_MO = 0   
+
+OUTPUT_FILE = "westside_2026-01-27.csv"
+
+
+HOME_CATEGORIES = {
+    "Living": "https://www.westside.com/collections/living",
+    "Kitchen": "https://www.westside.com/collections/kitchen",
+    "Bed": "https://www.westside.com/collections/bed",
+    "Bath": "https://www.westside.com/collections/bath",
+}
